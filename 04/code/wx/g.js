3091e2768d8d8e2a9b583d450a651841688b6bda
johnnynode <johnnynode@gmail.com>
2020-07-28 15:32:28 +0800

    update: 04

---
+++ 04/code/wx/g.js
@@ -0,0 +1,68 @@
+'use strict'
+
+var sha1 = require('sha1');
+var Wechat = require('./wechat');
+var getRawBody = require('raw-body');
+var util = require('./util');
+
+module.exports = function(opts) {
+    var wechat = new Wechat(opts); // 初始化实例
+
+    // 这个中间件用于处理事件推送数据的验证，用于返回信息的
+    return function*(next) {
+        // console.log(this.query);
+
+        var token = opts.token;
+        var signature = this.query.signature;
+        var nonce = this.query.nonce;
+        var timestamp = this.query.timestamp;
+        var echostr = this.query.echostr;
+
+        // 进行字典排序
+        var str = [token, timestamp, nonce].sort().join('');
+        var sha = sha1(str);
+
+        // 验证类型
+        var method = this.method;
+        if (method === 'GET') {
+            // 成功，则原样返回给微信
+            this.body = sha == signature ? echostr + '' : 'no!';
+        } else if (method === 'POST') {
+            // 处理用户点击事件或消息等POST数据
+            if (sha == signature) {
+                this.body = 'no';
+                return false;
+            }
+            var data = yield getRawBody(this.req, {
+                length: this.length,
+                limit: '1mb',
+                encoding: this.charset
+            });
+
+            var content = yield util.parseXMLAsync(data);
+            console.log(content);
+
+            var msg = util.formatMessage(content.xml);
+
+            this.weixin = msg;
+
+            yield handler.call(this, next);
+
+            wechat.reply.call(this);
+
+            /*
+            // 如果是事件
+            if (msg.MsgType = 'event') {
+                if (message.Event === 'subscribe') {
+                    var now = new Date().getTime();
+                    var content = '这里填写你要输入的内容';
+                    this.status = 200;
+                    this.type = 'application/xml';
+                    this.body = xml;
+                    return;
+                }
+            }
+            */
+        }
+    };
+}
---
+++ 04/code/wx/wechat.js
@@ -0,0 +1,80 @@
+'use strict'
+
+var Promise = require('bluebird');
+var request = Promise.promisify(require('request'));
+var util = require('./util');
+var prefix = 'https://api.weixin.qq.com/cgi-bin/';
+var api = {
+    accessToken: prefix + 'token?grant_type=client_credential'
+}
+
+// 用于生成实例 并 初始化工作 我们假设票据存储在文件中
+// 用于管理和微信交互的接口，票据的更新,存储,调用等
+function Wechat(opts) {
+    this.appId = opts.appId;
+    this.appSecret = opts.appSecret;
+    this.getAccessToken = opts.getAccessToken;
+    this.saveAccessToken = opts.saveAccessToken;
+
+    // 票据的读写
+    this.getAccessToken()
+        .then((data) => {
+            // 解析本地token, 如果没有，那么直接获取
+            try {
+                data = JSON.parse(data);
+            } catch (e) {
+                return this.updateAccessToken();
+            }
+
+            // 合法则使用，不合法则更新
+            if (this.isValidAccessToken(data)) {
+                Promise.resolve(data);
+            } else {
+                return this.updateAccessToken();
+            }
+        })
+        .then((data) => {
+            that.access_token = data.access_token;
+            that.expires_in = data.expires_in;
+        });
+}
+
+Wechat.prototype.isValidAccessToken = (data) => {
+    if (!data || !data.access_token || !data.expires_in) {
+        return false;
+    }
+
+    var expires_in = data.expires_in;
+    var now = new Date().getTime();
+
+    return now < expires_in;
+}
+
+Wechat.prototype.updateAccessToken = () => {
+    var appId = this.appId;
+    var appSecret = this.appSecret;
+    var url = api.accessToken + '&appid=' + appId + '&secret=' + appSecret;
+
+    return new Promise((resolve, reject) => {
+        request({ url: url, json: true })
+            .then((response) => {
+                var data = response[1];
+                var now = new Date().getTime();
+                var expires_in = now + (data.expires_in - 20) * 1000; // -20 为了提前更新token，给请求，响应留有余量
+                data.expires_in = expires_in;
+                resolve(data);
+            });
+    });
+}
+
+Wechat.prototype.reply = () => {
+    var content = this.body;
+    var msg = this.weixin;
+    var xml = util.tpl(content, msg);
+
+    this.status = 200;
+    this.type = 'application/xml';
+    this.body = xml;
+}
+
+module.exports = Wechat;
